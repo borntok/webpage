8b5ada82f28020ff1c377365d1bbb804033f7f5a
JU Yerin <yerin0823@naver.com>
2022-01-03 10:13:21 +0900

    관리자 설정

--- simpleBoard/board/admin.py
+++ simpleBoard/board/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import board
 
 # Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    # 관리자 페이지에서 화면에 보여지는 목록
+    list_display = ('createDate', 'user', 'subject')
+
+    #링크 새로 정의(제목을 클릭해서 상세페이지로 이동)
+    list_display_links = ['subject']
+    
+admin.site.register(board, BoardAdmin)
